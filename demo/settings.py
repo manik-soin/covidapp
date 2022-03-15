496852a0e0aa3e13e8199b4a9658aa6794696c35
manik-soin <41428798+manik-soin@users.noreply.github.com>
2022-03-16 03:57:42 +0800

    added allowed host

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x#wd)o9cpg4y$6$*0v!4lgqjh42+^j89i78eurd1jv(#2p805c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['radiant-cliffs-93327.herokuapp.com','localhost','127.0.0.1','https://radiant-cliffs-93327.herokuapp.com/']
 
 
 # Application definition
